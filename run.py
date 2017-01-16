30dbb81e86d591a83b6b0cd1463b4dc4eebd92df
Keith Johnson <keithyjohnson@gmail.com>
2017-01-16 12:31:26 -0500

    Initializing all the required components to train a skipgram: input/output vectors, their gradients, and cost.

--- run.py
+++ run.py
@@ -1,7 +1,19 @@
+from hyperparams import num_features, batch_size, window_size, num_noise
+from scripts.initialize_vectors import initialize_vectors
 import pandas as pd
+import numpy as np
 words_df = pd.read_csv(
     'cleaned_data/cleanedwords_from_all_tweets.csv',
     header=None,
     index_col=False,
     names=['body']
 )
+num_train = words_df.shape[0]
+
+embeddings     = initialize_vectors(words_df.shape[0], num_features)
+output_vectors = initialize_vectors(words_df.shape[0], num_features)
+
+cost = 0
+input_grad  = np.zeros(embeddings.shape)
+output_grad = np.zeros(output_vectors.shape)
+
