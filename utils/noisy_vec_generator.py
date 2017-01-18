5a51e6231ca2eeb7c2c3e2016568d5f7fdcc5162
Keith Johnson <keithyjohnson@gmail.com>
2017-01-18 17:28:38 -0500

    Moving NoisyVecGenerator to utils and having #sample return the index of a given words unique word vector as opposed to incorrectly returning the index of its first occurrence in the dataframe.

--- utils/noisy_vec_generator.py
+++ utils/noisy_vec_generator.py
@@ -1,28 +1,33 @@
 from scipy.stats import rv_discrete
 import numpy as np
+from ipdb import set_trace as st
 
 class NoisyVecGenerator():
-    def __init__(self, df):
+    def __init__(self, df, words_dict):
+        # Inputs:
+        # - df: dataframe where each row is a word in the order it appears in the original corpus
+        #       This is not single instance of each token, but however many times each one occurs
+        # - words_dict: key: a word, value, the word vector it maps to
         self.df     = df
+        self.words_dict = words_dict
+
+        # An descending ordered Series. The index is the word, the value is how many times it occurs.
         self.values = df.body.value_counts()
-        self.words  = self.values.index
         self.create_distribution()
 
     def sample(self):
         sampled_index = self.dist.rvs()
-        word = self.words[sampled_index]
+        word = self.values.index[sampled_index]
+        words_vector_index = self.words_dict[word]
 
-        # TODO just realized I have multiple word vectors for the same word.
-        df_index = self.df.body[self.df.body == word].index.tolist()[0]
-        return df_index, word
+        return words_vector_index, word
 
     def create_distribution(self):
-        num_unique = len(self.values)
+        num_unique = len(self.words_dict)
         num_train  = sum(self.values)
 
         xk = np.arange(num_unique)
         pk = (self.values.values / num_train)
-        
         # TODO Implement this particular distribution from http://papers.nips.cc/paper/5021-distributed-representations-of-words-and-phrases-and-their-compositionality.pdf
         # We investigated a  of choices for Pn(w) and found that the
         # unigram distribution U(w) raised to the 3/4rd power (i.e.,
