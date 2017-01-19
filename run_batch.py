88809def30c7ac289c22842c2031dcca20cf1469
Keith Johnson <keithyjohnson@gmail.com>
2017-01-19 18:00:37 -0500

    Fixing bug that caused the program to return the same loss for every batch run.  Though the batch loss is thrashing instead of steadily decreasing.

--- run_batch.py
+++ run_batch.py
@@ -10,12 +10,9 @@ def run_batch(input_vectors, output_vectors):
     context_word_indices = list(range(center_word_index - window_size, center_word_index + window_size + 1))
     context_word_indices.remove(center_word_index)
 
-    batch_input_grad = np.zeros(input_vectors.shape)
-    batch_output_grad = np.zeros(input_vectors.shape)
-
     [skipgram_loss,
      skipgram_input_grad_loss,
      skipgram_output_grad_loss
-    ] = skipgram(center_word_index, context_word_indices, batch_input_grad, batch_output_grad)
+    ] = skipgram(center_word_index, context_word_indices, input_vectors, output_vectors)
 
     return skipgram_loss, skipgram_input_grad_loss, skipgram_output_grad_loss
