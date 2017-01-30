4c4c1309b71094de6abd02d2d5d72d1dab416138
Keith Johnson <keithyjohnson@gmail.com>
2017-01-30 16:11:45 -0500

    Adding hparams as a param to def skipgram and passing it through to whatever cost_fn - TODO the softmax cost still needs to be updated to play well with an hparams hash.

--- algos/skipgram.py
+++ algos/skipgram.py
@@ -3,10 +3,9 @@ import numpy as np
 from utils.cost_functions import softmax_cost, negative_sampling_cost
 from utils.arrange_vectors import split_vecs, stack_vecs
 
-def skipgram(center_word_index, context_word_indices, word_vectors, cost_fn=negative_sampling_cost):
+def skipgram(center_word_index, context_word_indices, word_vectors, cost_fn=negative_sampling_cost, hparams={}):
     center_word_vector = word_vectors[center_word_index]
     input_vectors, output_vectors = split_vecs(word_vectors)
-
     input_grad  = np.zeros(input_vectors.shape)
     output_grad = np.zeros(output_vectors.shape)
     loss = 0
@@ -15,7 +14,7 @@ def skipgram(center_word_index, context_word_indices, word_vectors, cost_fn=nega
         [context_word_loss,
          input_grad_at_context_word,
          output_grad_at_context_word
-        ] = cost_fn(center_word_vector, context_word_idx, output_vectors, center_word_index=center_word_index)
+        ] = cost_fn(center_word_vector, center_word_index, output_vectors, center_word_index=center_word_index, hparams=hparams)
 
         loss                             += context_word_loss
         input_grad[center_word_index, :] += input_grad_at_context_word
